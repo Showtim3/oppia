55897def21bed5d24d3d8e18e50f0ea720fd5e2c
Showtim3 <coolrishabhrawat@gmail.com>
2019-07-31 18:46:19 +0530

    ADding test4

--- core/tests/protractor_desktop/showtime.js
+++ core/tests/protractor_desktop/showtime.js
@@ -24,32 +24,32 @@ var workflow = require('../protractor_utils/workflow.js');
 var LibraryPage = require('../protractor_utils/LibraryPage.js');
 
 describe('Play Later', function() {
-    var libraryPage = null;
+var libraryPage = null;
 
-    beforeEach(function() {
-        libraryPage = new LibraryPage.LibraryPage();
-    });
+beforeEach(function() {
+    libraryPage = new LibraryPage.LibraryPage();
+});
 
-    it('should display private and published explorations', function() {
+it('should display private and published explorations', function() {
 
-        var EXPLORATION_FRACTION = 'fraction';
-        var CATEGORY_ARCHITECTURE = 'Architecture';
-        var LANGUAGE_ENGLISH = 'English';
+    var EXPLORATION_FRACTION = 'fraction';
+    var CATEGORY_ARCHITECTURE = 'Architecture';
+    var LANGUAGE_ENGLISH = 'English';
 
-        users.createUser(
-            'feanor@publicationAndLibrary.com', 'feanorPublicationAndLibrary');
+    users.createUser(
+        'feanor@publicationAndLibrary.com', 'feanorPublicationAndLibrary');
 
-        users.login('feanor@publicationAndLibrary.com');
-        workflow.createAndPublishExploration(
-            EXPLORATION_FRACTION, CATEGORY_ARCHITECTURE,
-            'hold the light of the two trees', LANGUAGE_ENGLISH);
-        users.createUser(
-            'celebrimor@publicationAndLibrary.com', 'celebriorPublicationAndLibrary');
-        users.login('celebrimor@publicationAndLibrary.com');
+    users.login('feanor@publicationAndLibrary.com');
+    workflow.createAndPublishExploration(
+        EXPLORATION_FRACTION, CATEGORY_ARCHITECTURE,
+        'hold the light of the two trees', LANGUAGE_ENGLISH);
+    users.createUser(
+        'celebrimor@publicationAndLibrary.com', 'celebriorPublicationAndLibrary');
+    users.login('celebrimor@publicationAndLibrary.com');
 
 
-    });
-    afterEach(function() {
-        general.checkForConsoleErrors([])
-    })
+});
+afterEach(function() {
+    general.checkForConsoleErrors([])
+})
 });
