a347c99962930c45811a024ca8fb097f0c79649b
Showtim3 <coolrishabhrawat@gmail.com>
2019-07-31 18:44:47 +0530

    ADding test2

--- core/tests/protractor_desktop/showtime.js
+++ core/tests/protractor_desktop/showtime.js
@@ -17,39 +17,39 @@
  * the resultant display of explorations in the library.
  */
 
-var users = require('../protractor_utils/users.js');
-var workflow = require('../protractor_utils/workflow.js');
 
-var LibraryPage = require('../protractor_utils/LibraryPage.js');
+    var users = require('../protractor_utils/users.js');
+    var workflow = require('../protractor_utils/workflow.js');
 
-describe('Play Later', function() {
-    var libraryPage = null;
+    var LibraryPage = require('../protractor_utils/LibraryPage.js');
 
-    beforeEach(function() {
-        libraryPage = new LibraryPage.LibraryPage();
+    describe('Play Later', function() {
+        var libraryPage = null;
+
+        beforeEach(function() {
+            libraryPage = new LibraryPage.LibraryPage();
+        });
+
+        it('should display private and published explorations', function() {
+
+            var EXPLORATION_FRACTION = 'fraction';
+            var CATEGORY_ARCHITECTURE = 'Architecture';
+            var LANGUAGE_ENGLISH = 'English';
+
+            users.createUser(
+                'feanor@publicationAndLibrary.com', 'feanorPublicationAndLibrary');
+
+            users.login('feanor@publicationAndLibrary.com');
+            workflow.createAndPublishExploration(
+                EXPLORATION_FRACTION, CATEGORY_ARCHITECTURE,
+                'hold the light of the two trees', LANGUAGE_ENGLISH);
+            users.createUser(
+                'celebrimor@publicationAndLibrary.com', 'celebriorPublicationAndLibrary');
+            users.login('celebrimor@publicationAndLibrary.com');
+
+
+        });
+        afterEach(function() {
+            general.checkForConsoleErrors([])
+        })
     });
-
-    it('should display private and published explorations', function() {
-
-        var EXPLORATION_FRACTION = 'fraction';
-        var CATEGORY_ARCHITECTURE = 'Architecture';
-        var LANGUAGE_ENGLISH = 'English';
-
-        users.createUser(
-            'feanor@publicationAndLibrary.com', 'feanorPublicationAndLibrary');
-
-        users.login('feanor@publicationAndLibrary.com');
-        workflow.createAndPublishExploration(
-            EXPLORATION_FRACTION, CATEGORY_ARCHITECTURE,
-            'hold the light of the two trees', LANGUAGE_ENGLISH);
-        users.createUser(
-            'celebrimor@publicationAndLibrary.com', 'celebriorPublicationAndLibrary');
-        users.login('celebrimor@publicationAndLibrary.com');
-
-
-    });
-    afterEach(function() {
-        general.checkForConsoleErrors([])
-    })
-});
-
